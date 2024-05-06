061cb745dd2a69700c925b54f38cdfa391a3b312
kainem17 <kainelaperle10@gmail.com>
2024-05-06 15:45:17 -0400

    banner styling

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const Banner = () => {
     return (
-        <div className="bg-blue-700 w-full h-16 fixed top-0 flex items-center justify-center">
-            <h1 className="text-slate-50 text-xl">
+        <div className="bg-gradient-to-r from-blue-700 to-red-600
+        w-full h-16 flex flex-row items-center justify-center">
+            <h1 className="text-slate-50 text-xl font-semibold cursor-default">
                 NBA Mock Draft App
             </h1>
+            <button className="rounded border-white border-2 text-slate-50 font-semibold
+            pb-1 pt-1 px-2 hover:bg-blue-700 absolute right-10">
+                Log In
+            </button>
         </div>
     )
 }
--- client/src/components/Team.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Team = (props) => {
-    return (
-
-    )
-}
-
-export default Team;
